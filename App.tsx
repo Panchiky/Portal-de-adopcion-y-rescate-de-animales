ab01b13dfd4937273c1a2123b2768782e5f3c8bf
EliasBravoG <eliasismaelbravo59@gmail.com>
2026-09-21 01:18:24 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route, Navigate } from 'react-router-dom';
+
+/* Estilos de Ionic (Obligatorios) */
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Importación de tus pantallas de Auth y User */
+import Login from './pages/auth/Login';
+import CrearCuenta from './pages/auth/CrearCuenta';
+import Explorar from './pages/user/Explorar';
+import Postular from './pages/user/Postular';
+
+/* Importación de tu nueva pantalla de Admin */
+import SolicitudesAdmin from './pages/admin/SolicitudesAdmin';
+import AnimalesAdmin from './pages/admin/AnimalesAdmin';
+import AgendaAdmin from './pages/admin/AgendaAdmin';
+
+/* Importación del componente de rutas protegidas */
+import { ProtectedRoute } from './routes/ProtectedRoute';
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+       
+        {/* Rutas Publicas*/}
+      
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<CrearCuenta />} />
+
+   
+        {/* Rutas protegidas, usuario*/}
+     
+        <Route 
+          path="/catalogo" 
+          element={
+            <ProtectedRoute allowedRole="user">
+              <Explorar />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/animales/:id/postular" 
+          element={
+            <ProtectedRoute allowedRole="user">
+              <Postular />
+            </ProtectedRoute>
+          } 
+        />
+        
+      
+        {/* Rutas protegida admin*/}
+     
+        <Route 
+          path="/admin/solicitudes" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <SolicitudesAdmin />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/admin/animales" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <AnimalesAdmin />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/admin/agenda" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <AgendaAdmin />
+            </ProtectedRoute>
+          } 
+        />
+
+        {/* Redirección por defecto al abrir la app */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
